bbefbc2dd39f2e42432e1dbb5f43d7be1d6280fc
Влад <wlados2005west@gmail.com>
2023-12-20 00:04:29 +0500

    Помощь друга

--- js/main.js
+++ js/main.js
@@ -2,5 +2,7 @@ import {data} from './data.js';
 import {renderPictures} from './picture.js';
 import './forms.js';
 import './tagsPristine.js';
+import {initEffects} from './actions.js';
 
 renderPictures(data);
+initEffects();
